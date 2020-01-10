4c3e6a0a96ca25405193f5b8066ef1ab380944b8
GonzaloRuiz95 <40859032+GonzaloRuiz95@users.noreply.github.com>
2020-01-10 09:29:57 +0100

    User interface

--- TCP-IP Client Server Python/tcp_client.py
+++ TCP-IP Client Server Python/tcp_client.py
@@ -2,52 +2,143 @@
 import socket # Para usar sockets TCP
 import sys # Para admitir argumentos por línea de comandos
 import argparse as ap # Para facilitar el parseo de dichos argumentos. Usamos alias ap
+import os
+import PySimpleGUI as sg
+
+largest_frame = 100000
 
 if __name__ == '__main__':
-    # Definimos el parseador. Por convenio, los argumentos opcionales se encabezan con '--'
-    # Para más información, consulte https://docs.python.org/3/howto/argparse.html
+
+    # Defining parser. Por convenio, los argumentos opcionales se encabezan con '--'
+    # More info on https://docs.python.org/3/howto/argparse.html
     parser = ap.ArgumentParser(prog=sys.argv[0], description='Un cliente TCP de hola mundo')
     parser.add_argument('--ip', help='IP del servidor', default="127.0.0.1") # Por defecto usamos localhost
     # Los puertos por defecto están en el rango 0-1023. Podemos usar puertos a partir de ahí
-    parser.add_argument('--puerto', type=int, help='Puerto TCP de salida [1024-65535]', default=5005, choices=range(1024,65535), metavar='PUERTO (1024-65535)') 
+    parser.add_argument('--puerto', type=int, help='Puerto TCP de salida [1024-65535]', default=5004, choices=range(1024,65535), metavar='PUERTO (1024-65535)') 
     parser.add_argument('--tam_buf',  type=int, help='Longitud del buffer interno', default=4000)
     parser.add_argument('--mensaje', help='Mensaje a transmitir', default='Hola mundo')
     parser.add_argument('--DOWNLOAD_FILE')
-    
-    # Parseamos los argumentos de acuerdo al parser
+    parser.add_argument('--LIST_FILES', action='store_true')
+    parser.add_argument('--DELETE_FILE')
+    parser.add_argument('--MOVE_FILE', nargs='+') # nargs accepts 1 or more args
+
     args = parser.parse_args()
-    
-    download_args = sys.argv[1:3]
-    print(sys.argv[1:3])
-    
-    # Definimos socket e intentamos conectarnos al servidor.
-    # Primero definimos el socket (SOCK_STREAM es TCP) usa direcciones de internet (AF_INET)
+    client_args = sys.argv[1:4]
+
+    # Defining socket and trying to connect to the server
+    # First define socket (SOCK_STREAM is TCP) it uses inet directions (AF_INET)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.connect((args.ip, args.puerto)) # Intentamos conectarnos al servidor en el puerto especificado
-    
-    # Una vez conectados, debemos definir el protocolo del programa. 
-    # En este caso simple, el cliente manda un mensaje al que el servidor hace ECHO.
+    # Trying to connect to the server in the specific port
 
-    # Download File Option
-    download = '\n'.join(download_args)
-    print(download)
-    s.send(download.encode('ascii','ignore'))
+    sg.theme('TealMono')	# Add a touch of color
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Select option')],
+            [sg.Combo(['List files', 'Download File', 'Move', 'Delete', 'Upload'], 'Option')],
+            [sg.Text('List option does not need a filename')],
+            [sg.Text('Enter filename 1 (Download Delete or Move)'), sg.InputText()],
+            [sg.Text('Enter filename 2 (Only for move option)'), sg.InputText()],
+            [sg.Button('About')],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
 
-    mensaje = ''.join(args.mensaje) # Concatenamos el mensaje
+    window = sg.Window('TCP/IP Client', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event in (None, 'Cancel'):	# if user closes window or clicks cancel
+            break 
+        if event == "About":
+            sg.Popup("A program by Gonzalo Ruiz and Arturo López:\n ")
+
+        ui_option = values[0]
+        # List Option
+        if ui_option == "List files":
+            print("Listar")
+            list_args = "--LIST_FILES"
+            s.send(list_args.encode('ascii','ignore'))
+            data = s.recv(1024)
+            reponse = data.decode('ascii','ignore')
+            print(reponse)
+            #window = sg.Window('Get filename example', layout2)
+            sg.Popup("Files :", reponse)
+
+        # Download Option
+        elif ui_option == "Download File":
+            filename = 'copy-' + values[1]
+            val_list = ["--DOWNLOAD_FILE",values[1]]
+            download = '\n'.join(val_list)
+            s.send(download.encode('ascii','ignore'))
+            data = s.recv(1024)
+            reponse = data.decode('ascii','ignore')
+            if reponse == "ERROR":
+                print("File does not exists")
+                sg.Popup("File: " + filename + " does not exists")      
+            else:
+                size = int(reponse)
+                if size < largest_frame:
+                    s.send("ACK_DOWNLOAD".encode('ascii','ignore'))
+                    f = open(filename,'wb')
+                    l = s.recv(1024)
+                    while (l):
+                        f.write(l)
+                        l = s.recv(1024)
+                    f.close()
+                    sg.Popup("A copy of " + filename + " was created on client side")      
+
+        # Move Option
+        elif ui_option == "Move":
+            list_args = "--MOVE_FILE"
+            val_list = ["--MOVE_FILE",values[1], values[2]]
+            move = '\n'.join(val_list)
+            s.send(move.encode('ascii','ignore'))
+            data = s.recv(1024)
+            reponse = data.decode('ascii','ignore')
+            print(reponse)
+            sg.Popup(reponse + ": " + values[1] + " was renamed to " + values[2])
     
-    print ("Mandando el mensaje:", mensaje, 'a la IP:', args.ip)
-    
-    # Mandamos el dato en codificación ASCII (por defecto en Python3 las cadenas se codifican en UTF-8)
-    s.send(mensaje.encode("ascii", "ignore")) 
+        # Delete Option
+        elif ui_option == "Delete":
+            val_list = ["--DELETE_FILE",values[1], values[2]]
+            delete = '\n'.join(val_list)
+            s.send(delete.encode('ascii','ignore'))
+            data = s.recv(1024)
+            reponse = data.decode('ascii','ignore')
+            print(reponse)
+            if reponse == "SUCCESS":
+                sg.Popup(reponse + ": " + values[1] + " was deleted")
+            else:
+                sg.Popup(reponse + ": cant delete " + values[1])
+
+        elif ui_option == "Upload":
+            # (1) UPLOAD_FILE <nombre_fichero>
+            val_list = ["--UPLOAD_FILE", values[1], values[2]]
+            filename = values[1]
+            upload = '\n'.join(val_list)
+            s.send(upload.encode('ascii','ignore'))
+            data = s.recv(1024)
+            reponse = data.decode('ascii','ignore')
+            print(reponse)
+            # (3) LONGITUD
+            filesize = os.path.getsize(values[1])
+            s.send(bytes(str(filesize), 'utf8'))
+
+            data = s.recv(1024)
+            reponse = data.decode('ascii','ignore')
+
+            # Upload option
+            if reponse == "UPLOAD_ACK":
+                print("ACK Recibido. Enviando Contenido Del Fichero")
+                f = open(filename,'rb')
+                l = f.read(1024)
+                while (l):
+                    s.send(l)
+                    l = f.read(1024)
+                f.close()
+                sg.Popup("SUCCES: a copy of " + filename + " was created on server side")
+            else:
+                print("ERROR")
+               
+    window.close()
+    s.close()
+
         
-    #data = s.recv(args.tam_buf) # Esperamos el eco
-    
-    # Una vez acabado el intercambio de datos, debemos cerrar el socket
-    
-    
-
-
-
-    s.close() # Cerramos el socket
-
-    #print ("Datos recibidos:", data.decode("ascii"))
--- TCP-IP Client Server Python/tcp_server.py
+++ TCP-IP Client Server Python/tcp_server.py
@@ -5,36 +5,39 @@ import sys # Para admitir argumentos
 import argparse as ap # Para facilitar el parseo de dichos argumentos, le damos alias ap
 import os
 
+# LFS (Largest frame that can be received)
+largest_frame = 100000
+
 def getRequest(data):
     msj = ""
     # Converting Bytes to String
     for x in dat:
         msj += x
     # Split String to Array 
-    words = msj.split("\n")
-    return words
+    req = msj.split("\n")
+    return req
 
 if __name__ == '__main__':
     
     parser = ap.ArgumentParser(prog=sys.argv[0], description='Un servidor no concurrente TCP de hola mundo') # Definimos el parseador. Por convenio, los argumentos opcionales se encabezan con '--'
     parser.add_argument('--ip', help='IP del servidor', default='0.0.0.0')
-    parser.add_argument('--puerto', type=int, help='Puerto TCP de salida', default=5005, choices=range(1024,65535), metavar='1024-65535') 
+    parser.add_argument('--puerto', type=int, help='Puerto TCP de salida', default=5004, choices=range(1024,65535), metavar='1024-65535') 
     parser.add_argument('--tam_buf', type=int, help='Longitud del buffer interno', default=50) # Buffer corto para que responda antes
     
-    # Parseamos los argumentos de acuerdo al parser
+    # Parse arguments
     args = parser.parse_args(sys.argv[1:])
 
-    # Inicializacmos el servidor: empezamos a esperar conexiones. Para más información consulte: https://wiki.python.org/moin/HowTo/Sockets
+    # Inicialize server: waiting conn. more info: https://wiki.python.org/moin/HowTo/Sockets
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 1. Declaramos el socket
-    s.bind((args.ip, args.puerto)) # 2. Lo ligamos a una IP y puerto
-    s.listen(1) # 3. Podemos encolar hasta una solicitud
-    shutdown = False # Variable flag que controla si queremos finalizar el servidor. En el caso de que reciba 'shutdown' terminará su ejecución.
+    s.bind((args.ip, args.puerto)) # 2. ip and port associate
+    s.listen(1) # 3. Queue till one request
+    shutdown = False # Var flag controls server communication. If server receives'shutdown' the execution concludes.
     
     while not shutdown:
         conn, addr = s.accept()   # 4. Aceptamos cliente. Desempaquetamos la tupla de retorno en conexión y dirección del cliente
         print('Aceptado un cliente con (IP, puerto):', addr)
-        
         try:
+
             data = conn.recv(args.tam_buf)
             
             if not data: # Si no se reciben datos --> se ha desconectado
@@ -43,32 +46,94 @@ if __name__ == '__main__':
             dat = data.decode('ascii', 'ignore')
             user_request = getRequest(data)
 
-            # DOWNLOAD FILE 
-            if user_request[0] == "--DOWNLOAD_FILE":
+            if user_request[0] == "--LIST_FILES":
+                os.system('CLS')
+                print("# # # List files # # #")
+                files = "\n".join(os.listdir(os.path.expanduser('.')))
+                print(files)
+                conn.send(files.encode('ascii','ignore'))
+
+            # Download file 
+            elif user_request[0] == "--DOWNLOAD_FILE":
                 os.system('cls')
-                print("# # # Download File # # #")
                 filename = user_request[1]
-                print(filename)
                 if os.path.exists(filename):
                     print("File exists")
                     file_size = os.path.getsize(filename)
-                    print(file_size)
-                    f = open(filename,'rb')
-                    l = f.read(1024)
-                    while(l):
-                        #conn.send(l)
-                        print('Sent')
+                    conn.send(bytes(str(file_size), 'utf8'))
+                    data = conn.recv(1024)
+                    reponse = data.decode('ascii','ignore')
+                    if reponse == "ACK_DOWNLOAD":
+                        f = open(filename,'rb')
                         l = f.read(1024)
-                    f.close
+                        while (l):
+                            conn.send(l)
+                            l = f.read(1024)
+                        f.close()
+                else:
+                    print("File does not exists")
+                    conn.send("ERROR".encode('ascii','ignore'))
 
-                    #with open(filename,'wb') as f: ESTO VA EN EL CLIENTE
-                        #print('file opened')
+            
+            # Delete file
+            elif user_request[0] == "--DELETE_FILE":
+                os.system('CLS')
+                print("# # # Delete File # # #")
+                filename = user_request[1]
+                if os.path.exists(filename) and filename != "tcp_server.py" and filename != "tcp_client.py":
+                    print("File " + filename + " was removed")
+                    os.remove(filename)
+                    reponse = "SUCCESS"
+                else:
+                    print("No existe")
+                    reponse = "ERROR"
+            
+                conn.send(reponse.encode('ascii','ignore'))
 
+            elif user_request[0] == "--MOVE_FILE":
+                os.system('CLS')
+                print("# # # Move File # # #")
+                filename1 = user_request[1]
+                filename2 = user_request[2]
 
+                if os.path.exists(filename1) and not os.path.exists(filename2):
+                    os.rename(filename1,filename2)
+                    print("File was renamed")
+                    reponse = "SUCCES"
                 else:
                     print("Error")
+                    reponse = "ERROR"
 
-            conn.send(data)
+                conn.send(reponse.encode('ascii','ignore'))
+            
+            elif user_request[0] == "--UPLOAD_FILE":
+                os.system('CLS')
+                print("# # # UPLOAD FILE # # #")
+                # (2) UPLOAD_ACK
+                reponse = "UPLOAD_ACK"
+                conn.send(reponse.encode('ascii','ignore'))
+                reponse = conn.recv(1024)
+                size = reponse.decode('ascii','ignore')
+                print(size)
+                tam = int(size)
+                # (4) UPLOAD_ACK
+                if tam < largest_frame:
+                    reponse = "UPLOAD_ACK"
+                    conn.send(reponse.encode('ascii','ignore'))
+                    f = open("Descarga.txt",'wb')
+                    l = conn.recv(1024)
+                    while (l):
+                        f.write(l)
+                        l = conn.recv(1024)
+                    f.close()
+                else:
+                    conn.send("ERROR".encode('ascii','ignore'))
+                
+            else:
+                print("Error")
+                reponse = "INVALID COMMAND"
+                conn.send(reponse.encode('ascii','ignore'))
+
             conn.close() 
             
             if data == b'shutdown':
